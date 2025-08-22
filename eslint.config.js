35cb2a76070b923bb863147de6df56481b50f1f5
johnsulf <erlendjohns@gmail.com>
2025-08-22 13:16:40 +0200

    adds recommended content to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,50 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import unused from "eslint-plugin-unused-imports";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { globalIgnores } from "eslint/config";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  // Ignore build artifacts
+  globalIgnores(["dist", "coverage"]),
+
+  // Base JS + TS recommendations
+  js.configs.recommended,
+
+  // Type-aware rules
+  ...tseslint.configs.recommendedTypeChecked,
+
+  // React-specific rules
+  reactHooks.configs["recommended-latest"],
+  reactRefresh.configs.vite,
+
+  // Project glues
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        // Lets TS-ESLint find tsconfig automatically
+        project: true,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      "unused-imports": unused,
+    },
+    rules: {
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "unused-imports/no-unused-imports": "error",
     },
   },
-])
+
+  // Disable stylistic rules that conflict with Prettier
+  eslintConfigPrettier
+);
