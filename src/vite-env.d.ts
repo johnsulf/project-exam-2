374a764f8283b45a78acfc2be0b67aa79361a343
Erlend Johnsen <96027411+johnsulf@users.noreply.github.com>
2025-09-09 16:49:36 +0200

    Feature/zod (#3)
    * adds zod package and env file
    
    * adds env files
    
    * adds test

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_APP_ENV?: "development" | "test" | "production";
+  readonly VITE_DEBUG?: string | boolean;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
