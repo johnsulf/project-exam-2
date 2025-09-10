7b563e76304ed99d0154a5dc530531374b6ba67f
Erlend Johnsen <96027411+johnsulf@users.noreply.github.com>
2025-09-10 09:34:30 +0200

    Feature/tanstack (#5)
    * adds queryKeys and queryClient
    
    * adds queryclientprovider to appproviders
    
    * updates endpoints to accepts the signal passed by query
    
    * adds hooks for venues
    
    * adds simple venues page with api results

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,12 +1,20 @@
 import type { PropsWithChildren } from "react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { makeQueryClient } from "@/lib/queryClient";
+
+const client = makeQueryClient();
 
 export function AppProviders({ children }: PropsWithChildren) {
   return (
     <TooltipProvider delayDuration={200} skipDelayDuration={400}>
-      {children}
-      <Toaster />
+      <QueryClientProvider client={client}>
+        {children}
+        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+        <Toaster />
+      </QueryClientProvider>
     </TooltipProvider>
   );
 }
