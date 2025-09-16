bfd0104cd49b793e9677f9e6eb2481a5bd9a9062
johnsulf <erlendjohns@gmail.com>
2025-09-16 12:18:11 +0200

    adds debug button for crash testing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,22 @@
 import ApiSmokeTest from "@/components/test/ApiSmokeTest";
 import ComponentsTester from "@/components/test/ComponentsTester";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  // for crash testing
+  const [crash, setCrash] = useState(false);
+  if (crash) {
+    throw new Error("Crash!");
+  }
   return (
     <>
       <h1>Home</h1>
       <ComponentsTester />
-      <ApiSmokeTest />
+      <div className="flex gap-3 my-2">
+        <ApiSmokeTest />
+        <Button onClick={() => setCrash(true)}>Crash page</Button>
+      </div>
     </>
   );
 }
