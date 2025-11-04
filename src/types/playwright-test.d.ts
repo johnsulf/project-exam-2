2fbe27e6deb81bae783e927e61b1528c2d10a277
johnsulf <erlendjohns@gmail.com>
2025-11-04 10:38:29 +0100

    adds test script

--- src/types/playwright-test.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "@playwright/test" {
-  export type PlaywrightTestConfig = Record<string, unknown>;
-  export const test: unknown;
-  export const expect: unknown;
-  export const devices: Record<string, unknown>;
-  export function defineConfig<T extends PlaywrightTestConfig>(config: T): T;
-}
